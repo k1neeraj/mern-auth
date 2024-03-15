3a05eb748ed9a06e9f747838346de5c902ba7572
Neeraj Kumar <k1_neeraj@yahoo.com>
2024-03-15 16:42:26 +0530

    create user model

--- server/index.js
+++ server/index.js
@@ -3,11 +3,14 @@ import mongoose from "mongoose";
 import dotenv from "dotenv";
 dotenv.config();
 
-mongoose.connect(process.env.MONGO).then(() => {
-  console.log("Connected to MongoDB");
-}).catch((err)=>{
-    console.log(err)
-});
+mongoose
+  .connect(process.env.MONGO)
+  .then(() => {
+    console.log("Connected to MongoDB");
+  })
+  .catch((err) => {
+    console.log(err);
+  });
 
 const app = express();
 
