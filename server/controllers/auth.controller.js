23583d80200778be9ae0f134090df25ca035090d
Neeraj Kumar <k1_neeraj@yahoo.com>
2024-03-18 01:58:44 +0530

    created sign in api route

--- server/controllers/auth.controller.js
+++ server/controllers/auth.controller.js
@@ -1,5 +1,7 @@
 import User from "../models/user.model.js";
 import bcryptjs from "bcryptjs";
+import { errorHandler } from "../utils/error.js";
+import jwt from "jsonwebtoken";
 
 export const signup = async (req, res, next) => {
   const { username, email, password } = req.body;
@@ -14,6 +16,25 @@ export const signup = async (req, res, next) => {
     res.status(201).json({ message: "User created successfully" });
   } catch (error) {
     // res.status(500).json(error.message);
+    next(error);
+  }
+};
+
+export const signin = async (req, res, next) => {
+  const { email, password } = req.body;
+  try {
+    const vaildUser = await User.findOne({ email });
+    if (!vaildUser) return next(errorHandler(404, "User not found"));
+    const validPassword = bcryptjs.compareSync(password, vaildUser.password);
+    if (!validPassword) return next(errorHandler(401, "Wrong credentials"));
+    const token = jwt.sign({ id: vaildUser._id, jwt }, process.env.JWT_SECRET);
+    const { password: hashedPassword, ...userdata } = vaildUser._doc;
+    const expiryDate = new Date(Date.now() + 3600000); // 1 hour
+    res
+      .cookie("access_token", token, { httpOnly: true, expires: expiryDate })
+      .status(200)
+      .json(userdata);
+  } catch (error) {
     next(error);
   }
 };
